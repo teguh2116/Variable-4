00dd3abc887fd7d5b520d939957740d574ec44a3
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:26:15 +0700

    Add files via upload

---
+++ Variable Lokal 4.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int tambah(int a, int b);
+int main()
+{
+    int hasil;
+    hasil = tambah(2,3);
+    printf("hasil=%d",hasil);
+    getch();
+}
+int tambah (int a, int b)
+{
+    int c;
+    c = a=b;
+    {
+        float c;
+        c=100;
+    }
+return(c);
+}
